1e59de92596f5007f08d3914e875f8041e049d5b
Kronos <dimamitru@gmail.com>
2023-11-08 17:12:03 +0200

    Get all categories

--- BookWormWeb/Controllers/CategoryController.cs
+++ BookWormWeb/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookWormWeb.Data;
+using BookWormWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookWormWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            //Can be used "var" instead of List<Category>.
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
