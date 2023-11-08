001189ae1f566480d570cdb98ead3318d0900662
Kronos <dimamitru@gmail.com>
2023-11-08 16:13:23 +0200

    Seed Category Table

--- BookWormWeb/Data/ApplicationDbContext.cs
+++ BookWormWeb/Data/ApplicationDbContext.cs
@@ -14,5 +14,16 @@ namespace BookWormWeb.Data
 
         //PMC: add-migration AddCategoryTableToDb => update-database
         public DbSet<Category> Categories { get; set; }
+
+
+        //Seeding data to db => PMC: add-migration ... => update database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1},
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2},
+                new Category { Id = 3, Name = "History", DisplayOrder = 3}
+                );
+        }
     }
 }
--- BookWormWeb/Migrations/20231108140801_SeedCategoryTabel.Designer.cs
+++ BookWormWeb/Migrations/20231108140801_SeedCategoryTabel.Designer.cs
@@ -3,6 +3,7 @@ using BookWormWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookWormWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231108140801_SeedCategoryTabel")]
+    partial class SeedCategoryTabel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
